f18f37c5f63f706d8bdc7693bfd7b5aff7c203e8
Matt Kelly <mattk@mattkelly.me.uk>
2020-03-19 08:28:27 +0000

    add Todo DynmoDb table, GraphQL queries, AppSync API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import * as queries from './graphql/queries';
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
+
+    (async () => {
+      const allTodos = await API.graphql(graphqlOperation(queries.listTodos));
+
+      console.log(allTodos);
+    })();
+    
+
     return (
       <div className="App">
         <header className="App-header">
